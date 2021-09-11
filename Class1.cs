4202a385f02c44c8d7161eedb01b0f67725ae4c2
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-11 14:25:29 -0400

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -7,6 +7,9 @@ public class Class1
         Random r = new Random()
             int winNum = r.Next(0, 100);
         bool win = false;
-
+        do
+        {
+            Console.Write("Guess a number in between 0-100");
+        } while (win == false);
     }
 }
